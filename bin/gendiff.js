6390f3958f342d768d21bb04786d9dee4940e775
fairwind2k <fairwind@md2k.net>
2023-09-12 15:21:54 +0200

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,14 +4,9 @@ import { program } from 'commander';
 program
   .name('gendiff.js')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-
-// program.command('-h')
-//   .option('--first')
-//   .option('-s, --separator <char>')
-//   .action((str, options) => {
-//     const limit = options.first ? 1 : undefined;
-//     console.log(str.split(options.separator, limit));
-//   });
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
