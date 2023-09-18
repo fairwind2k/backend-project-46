c0fef8ec83fa9ff54b503310cabba0d4967aaa14
fairwind2k <fairwind@md2k.net>
2023-09-18 22:49:38 +0200

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import gendiff from '../src/diff.js';
 
 program
   .name('gendiff.js')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .command('parser <filepath1> <filepath2>', 'print arrays');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log('234'); // stub
+    console.log(gendiff(filepath1, filepath2));
+  });
 
 program.parse(process.argv);
