808259bc4f74541eed4bfa6c2f3ab862fa23b6ee
fairwind2k <fairwind@md2k.net>
2023-09-20 22:44:16 +0200

    fix func

--- src/diff.js
+++ src/diff.js
@@ -8,22 +8,22 @@ const gendiff = (filepath1, filepath2, format = 'plain') => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const keys = _.union(keys1, keys2).sort();
-  const str = [];
-  for (const key of keys) {
+  const result = keys.reduce((acc, key) => {
     if (!Object.hasOwn(data1, key)) {
-      str.push(`  + ${key}: ${data2[key]}`);
+      acc.push(`  + ${key}: ${data2[key]}`);
     } else if (!Object.hasOwn(data2, key)) {
-      str.push(`  - ${key}: ${data1[key]}`);
+      acc.push(`  - ${key}: ${data1[key]}`);
     } else if (Object.hasOwn(data1, key)) {
       if (data1[key] === data2[key]) {
-        str.push(`    ${key}: ${data1[key]}`);
+        acc.push(`    ${key}: ${data1[key]}`);
       } else {
-        str.push(`  - ${key}: ${data1[key]}`);
-        str.push(`  + ${key}: ${data2[key]}`);
+        acc.push(`  - ${key}: ${data1[key]}`);
+        acc.push(`  + ${key}: ${data2[key]}`);
       }
     }
-  }
-  return ['{', ...str, '}'].join('\n');
+    return acc;
+  }, []);
+  return ['{', ...result, '}'].join('\n');
 };
 
 export default gendiff;
