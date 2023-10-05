a14de157a3bfffcdc7091d45dd80d663f96c6bdc
fairwind2k <fairwind@md2k.net>
2023-10-05 23:56:21 +0200

    update diff.tests.js: refactor code

--- __tests__/diff.tests.js
+++ __tests__/diff.tests.js
@@ -1,11 +1,20 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 import gendiff from '../src/diff.js';
+import expectString from './__fixtures__/expected_result.js';
 
-const path1 = '__tests__/__fixtures__/file1.json';
-const path2 = '__tests__/__fixtures__/file2.json';
-const expectString = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '../fixtures', filename);
+const pathJson1 = getFixturePath('file1.json');
+const pathJson2 = getFixturePath('file2.json');
+
+// const path1 = '__tests__/__fixtures__/file1.json';
+// const path2 = '__tests__/__fixtures__/file2.json';
 
 test('testing function gendiff(filepath1, filepath2)', () => {
-  expect(gendiff(path1, path2)).toBe(expectString);
+  expect(gendiff(pathJson1, pathJson2)).toBe(expectString);
 });
