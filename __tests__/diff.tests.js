fe73c04012a8874b907fbb19d7cd55d9b3dab349
fairwind2k <fairwind@md2k.net>
2023-10-06 19:22:27 +0200

    refactor code in diff.tests.js

--- __tests__/diff.tests.js
+++ __tests__/diff.tests.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 
 import gendiff from '../src/diff.js';
 import expectString from './__fixtures__/expected_result.js';
@@ -8,12 +8,9 @@ import expectString from './__fixtures__/expected_result.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '../__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '/__fixtures__', filename);
 const pathJson1 = getFixturePath('file1.json');
 const pathJson2 = getFixturePath('file2.json');
-
-// const path1 = '__tests__/__fixtures__/file1.json';
-// const path2 = '__tests__/__fixtures__/file2.json';
 
 test('testing function gendiff(filepath1, filepath2)', () => {
   expect(gendiff(pathJson1, pathJson2)).toBe(expectString);
