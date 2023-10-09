c3bca7b9333b33b8dfb1f19294c2169b52ef3dd9
fairwind2k <fairwind@md2k.net>
2023-10-09 23:41:35 +0200

    refactor code  __tests__/diff.tests.js

--- __tests__/diff.tests.js
+++ __tests__/diff.tests.js
@@ -3,12 +3,12 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 import gendiff from '../src/diff.js';
-import expectString from './__fixtures__/expected_result.js';
+import expectString from '../__fixtures__/expected_result.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => join(__dirname, '/__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '../__fixtures__', filename);
 const pathJson1 = getFixturePath('file1.json');
 const pathJson2 = getFixturePath('file2.json');
 
