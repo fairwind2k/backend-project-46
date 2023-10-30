6449cd3cd11ff871c5f37afe5f23d5ea010b1a61
fairwind2k <fairwind@md2k.net>
2023-10-30 18:36:39 +0100

    refactor code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,10 +7,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+  .action((filepath1, filepath2, option) => {
+    console.log('123', option.format);
+    console.log(gendiff(filepath1, filepath2, option.format));
   });
 
 program.parse(process.argv);
