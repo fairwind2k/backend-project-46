42e4f6bc00136f38f21bdf77f0c03f1483bd85fa
fairwind2k <fairwind@md2k.net>
2023-11-13 23:12:38 +0100

    refactor code

--- src/diff.js
+++ src/diff.js
@@ -1,28 +1,31 @@
-import _ from 'lodash';
 import parseFile from './helpers/parsers.js';
+import makeTree from './helpers/makeTree.js';
+import makeStylish from './makeStylish.js';
+
+// const stilished = (keys, data1, data2) => {
+//   const result = keys.reduce((acc, key) => {
+//     if (!Object.hasOwn(data1, key)) {
+//       acc.push(`  + ${key}: ${data2[key]}`);
+//     } else if (!Object.hasOwn(data2, key)) {
+//       acc.push(`  - ${key}: ${data1[key]}`);
+//     } else if (Object.hasOwn(data1, key)) {
+//       if (data1[key] === data2[key]) {
+//         acc.push(`    ${key}: ${data1[key]}`);
+//       } else {
+//         acc.push(`  - ${key}: ${data1[key]}`);
+//         acc.push(`  + ${key}: ${data2[key]}`);
+//       }
+//     }
+//     return acc;
+//   }, []);
+//   return ['{', ...result, '}'].join('\n');
+// };
 
 const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.union(keys1, keys2).sort();
-  const result = keys.reduce((acc, key) => {
-    if (!Object.hasOwn(data1, key)) {
-      acc.push(`  + ${key}: ${data2[key]}`);
-    } else if (!Object.hasOwn(data2, key)) {
-      acc.push(`  - ${key}: ${data1[key]}`);
-    } else if (Object.hasOwn(data1, key)) {
-      if (data1[key] === data2[key]) {
-        acc.push(`    ${key}: ${data1[key]}`);
-      } else {
-        acc.push(`  - ${key}: ${data1[key]}`);
-        acc.push(`  + ${key}: ${data2[key]}`);
-      }
-    }
-    return acc;
-  }, []);
-  return ['{', ...result, '}'].join('\n');
+  const tree = makeTree(data1, data2);
+  return makeStylish(tree, data1, data2);
 };
 
 export default gendiff;
