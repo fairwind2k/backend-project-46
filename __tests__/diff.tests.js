55f841a4a9a89fff485fb6b0c37286932616018f
fairwind2k <fairwind@md2k.net>
2023-11-13 23:12:10 +0100

    update diff.tests.js

--- __tests__/diff.tests.js
+++ __tests__/diff.tests.js
@@ -9,17 +9,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '../__fixtures__', filename);
-const pathJson1 = getFixturePath('file1.json');
-const pathJson2 = getFixturePath('file2.json');
-const pathYaml1 = getFixturePath('file1.yaml');
-const pathYaml2 = getFixturePath('file2.yaml');
-const pathYml1 = getFixturePath('file1.yml');
-const pathYml2 = getFixturePath('file2.yml');
+// const pathJson1 = getFixturePath('file1.json');
+// const pathJson2 = getFixturePath('file2.json');
+// const pathYaml1 = getFixturePath('file1.yaml');
+// const pathYaml2 = getFixturePath('file2.yaml');
+// const pathYml1 = getFixturePath('file1.yml');
+// const pathYml2 = getFixturePath('file2.yml');
 
-const expectString = readFileSync(getFixturePath('expected.txt'), 'utf8');
+const testPath1 = getFixturePath('file10.json');
+const testPath2 = getFixturePath('file20.json');
 
-test('testing function gendiff(filepath1, filepath2, formatName)', () => {
-  expect(gendiff(pathJson1, pathJson2, 'stylish')).toBe(expectString);
-  expect(gendiff(pathYaml1, pathYaml2, 'stylish')).toBe(expectString);
-  expect(gendiff(pathYml1, pathYml2, 'stylish')).toBe(expectString);
+// const expectString = readFileSync(getFixturePath('expected.txt'), 'utf8');
+
+const expectString = readFileSync(getFixturePath('expected2.txt'), 'utf8');
+
+test('testing function plain gendiff(filepath1, filepath2)', () => {
+  expect(gendiff(testPath1, testPath2)).toBe(expectString);
 });
+
+// test('testing function gendiff(filepath1, filepath2, formatName)', () => {
+//   expect(gendiff(pathJson1, pathJson2, 'stylish')).toBe(expectString);
+//   expect(gendiff(pathYaml1, pathYaml2, 'stylish')).toBe(expectString);
+//   expect(gendiff(pathYml1, pathYml2, 'stylish')).toBe(expectString);
+// });
