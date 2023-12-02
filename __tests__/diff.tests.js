3d48d3685b265d10a11e62fb4ee5c2ff190b6bd4
fairwind2k <fairwind@md2k.net>
2023-12-02 12:31:16 +0100

    update diff.tests.js

--- __tests__/diff.tests.js
+++ __tests__/diff.tests.js
@@ -17,9 +17,16 @@ const pathYml1 = getFixturePath('file1.yml');
 const pathYml2 = getFixturePath('file2.yml');
 
 const expectString = readFileSync(getFixturePath('expected.txt'), 'utf8');
+const expectStringPlain = readFileSync(getFixturePath('expected_plain.txt'), 'utf8');
 
 test('testing function gendiff(filepath1, filepath2, formatName)', () => {
   expect(gendiff(pathJson1, pathJson2, 'stylish')).toBe(expectString);
   expect(gendiff(pathYaml1, pathYaml2, 'stylish')).toBe(expectString);
   expect(gendiff(pathYml1, pathYml2, 'stylish')).toBe(expectString);
+});
+
+test('testing function gendiff(filepath1, filepath2, formatName = "plain")', () => {
+  expect(gendiff(pathJson1, pathJson2, 'plain')).toBe(expectStringPlain);
+  expect(gendiff(pathYaml1, pathYaml2, 'plain')).toBe(expectStringPlain);
+  expect(gendiff(pathYml1, pathYml2, 'plain')).toBe(expectStringPlain);
 });
