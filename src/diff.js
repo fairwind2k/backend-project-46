41c06661585934a877fec1e367ba5b0a0bb7da9d
fairwind2k <fairwind@md2k.net>
2023-12-06 21:27:41 +0100

    refactor code

--- src/diff.js
+++ src/diff.js
@@ -1,16 +1,16 @@
 import parseFile from './helpers/parsers.js';
 import makeTree from './helpers/makeTree.js';
 import makeStylish from './formatters/makeStylish.js';
+import makePlain from './formatters/makePlain.js';
 
 const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
   const tree = makeTree(data1, data2);
-  let res = '';
-  if (formatName === 'stylish') {
-    res = makeStylish(tree, data1, data2);
+  if (formatName === 'plain') {
+    return makePlain(tree);
   }
-  return res;
+  return makeStylish(tree);
 };
 
 export default gendiff;
