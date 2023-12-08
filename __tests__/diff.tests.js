9a3e24adfa307f3eb376800734832e685b837b84
fairwind2k <fairwind@md2k.net>
2023-12-08 14:55:40 +0100

    fix the imports

--- __tests__/diff.tests.js
+++ __tests__/diff.tests.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readFileSync } from 'node:fs';
 
-import gendiff from '../src/diff.js';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
