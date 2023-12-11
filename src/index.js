2ac4d40ace4096225fc8a5ab586f3ab4d730df87
fairwind2k <fairwind@md2k.net>
2023-12-11 01:43:29 +0100

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
+import path from 'path';
+import fs from 'fs';
+import makePath from './helpers/makePath.js';
 import parseFile from './helpers/parsers.js';
 import makeTree from './helpers/makeTree.js';
 import makeStylish from './formatters/makeStylish.js';
 import makePlain from './formatters/makePlain.js';
-import makeJson from './formatters/makeJson.js';
 
-const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
-  const tree = makeTree(data1, data2);
-  if (formatName === 'plain') {
+const getPath = (filepath) => {
+  const unparsedData = fs.readFileSync(makePath(filepath));
+  return unparsedData;
+};
+
+const getExtension = (filepath) => path.extname(filepath);
+
+const getOutput = (tree, formatter) => {
+  if (formatter === 'plain') {
     return makePlain(tree);
   }
-  if (formatName === 'json') {
-    return makeJson(tree);
+  if (formatter === 'json') {
+    return JSON.stringify(tree);
   }
   return makeStylish(tree);
+};
+
+const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const path1 = getPath(filepath1);
+  const path2 = getPath(filepath2);
+  const extension1 = getExtension(filepath1);
+  const extension2 = getExtension(filepath2);
+  const data1 = parseFile(path1, extension1);
+  const data2 = parseFile(path2, extension2);
+  const diffTree = makeTree(data1, data2);
+  return getOutput(diffTree, formatName);
 };
 
 export default gendiff;
