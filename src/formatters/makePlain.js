f31e24cc21040314595344f7e3292ae67e25f374
fairwind2k <fairwind@md2k.net>
2023-12-13 19:55:37 +0100

    fix code

--- src/formatters/makePlain.js
+++ src/formatters/makePlain.js
@@ -1,35 +1,36 @@
 import _ from 'lodash';
 
+const getString = (nodeValue) => {
+  if (_.isObject(nodeValue)) {
+    return '[complex value]';
+  }
+  if (typeof (nodeValue) === 'string') {
+    return `'${nodeValue}'`;
+  }
+  return `${nodeValue}`;
+};
+
 const makePlain = (data) => {
   const iter = (currentValue, acc, depth) => {
-    const getString = (nodeValue) => {
-      if (_.isObject(nodeValue)) {
-        return '[complex value]';
-      }
-      if (typeof (nodeValue) === 'string') {
-        return `'${nodeValue}'`;
-      }
-      return `${nodeValue}`;
-    };
-
     const entries = currentValue.map((node) => {
       const {
         key, value, value1, value2, status, children,
       } = node;
       const currentKey = [...acc, key].join('.');
-      if (status === 'added') {
-        return `Property '${currentKey}' was ${status} with value: ${getString(value)}`;
+      switch (status) {
+        case 'added':
+          return `Property '${currentKey}' was ${status} with value: ${getString(value)}`;
+        case 'removed':
+          return `Property '${currentKey}' was ${status}`;
+        case 'changed':
+          return `Property '${currentKey}' was updated. From ${getString(value1)} to ${getString(value2)}`;
+        case 'unchanged':
+          return '';
+        case 'nested':
+          return iter(children, [...acc, key], depth + 1);
+        default:
+          throw new Error("Can't found a status!");
       }
-      if (status === 'removed') {
-        return `Property '${currentKey}' was ${status}`;
-      }
-      if (status === 'changed') {
-        return `Property '${currentKey}' was updated. From ${getString(value1)} to ${getString(value2)}`;
-      }
-      if (status === 'nested' && children !== undefined) {
-        return iter(children, [...acc, key], depth + 1);
-      }
-      return '';
     });
     return entries.filter((string) => (string !== '')).join('\n');
   };
